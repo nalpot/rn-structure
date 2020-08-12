b74866025c8b618ee427cf9b875371e3a263d0dc
Rifqi Maulana <30535103+nalpot@users.noreply.github.com>
2020-08-12 16:32:11 +0700

    Update index.js

--- src/screens/welcome/index.js
+++ src/screens/welcome/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import {connect} from 'react-redux';
 import Text from '@app/libs/Text';
+import {setLoadingSplash} from '@app/redux/reducers/Splash';
 
 const SplashScreen: () => React$Node = React.forwardRef((props, ref) => {
+    useEffect(() => {
+        setTimeout(props.setLoading.bind(this, false), 2000);
+    }, []);
+
     return (
         <View>
             <Text>Hello World</Text>
@@ -15,6 +20,8 @@ const mapStateToProps = (state, props) => {
     return {};
 };
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+    setLoading: (b) => (dispatch) => setLoadingSplash(b),
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(SplashScreen);
