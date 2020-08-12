8409c00347236029165c15895a69566557fbf4bf
Rifqi Maulana <30535103+nalpot@users.noreply.github.com>
2020-08-12 16:28:50 +0700

    Update index.js

--- src/network/index.js
+++ src/network/index.js
@@ -1,5 +1,5 @@
 import {emit, socketApp} from './socket';
 
 export async function login(params) {
-  return emit(socketApp, 'your event', params);  
+    return emit(socketApp, 'your event', params);
 }
